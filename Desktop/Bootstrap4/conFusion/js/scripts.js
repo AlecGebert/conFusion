68ae8c1e2d65236593e806fbb94dba413e172ae7
Oleg <oleg_goncharov@inbox.ru>
2021-10-01 13:15:07 +0300

    npm scriptspart1

---
+++ Desktop/Bootstrap4/conFusion/js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $('#mycarousel').carousel({
+        interval: 2000
+    });
+
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+
+        } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+
+    });
+
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    });
+
+    $("#closeLoginButton").click(function () {
+        $("#loginModal").modal("hide");
+    });
+
+    $("#cancelLoginButton").click(function () {
+        $("#loginModal").modal("hide");
+    });
+
+
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+
+    $("#closeReserveButton").click(function () {
+        $("#reserveModal").modal("hide");
+    });
+
+    $("#cancelReserveButton").click(function () {
+        $("#reserveModal").modal("hide");
+    });
+
+});
